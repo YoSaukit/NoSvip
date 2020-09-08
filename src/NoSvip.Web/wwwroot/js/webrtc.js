f94cc9f6fc80b77d30ef3647071c229401d29337
tfcye <whoisye@outlook.com>
2020-09-08 23:27:48 +0800

    replace ice server

--- src/NoSvip.Web/wwwroot/js/webrtc.js
+++ src/NoSvip.Web/wwwroot/js/webrtc.js
@@ -8,7 +8,7 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/WebRTCHub").build(
 
 const configuration = {
    'iceServers': [{
-     'urls': 'stun:stun.l.google.com:19302'
+     'urls': 'stun:stun.xten.com:3478'
    }]
  };
 const peerConn = new RTCPeerConnection(configuration);
